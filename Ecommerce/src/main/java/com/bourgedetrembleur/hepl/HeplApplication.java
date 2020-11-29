a13858c69c882527771aee469a6dffaef69aefad
ArthurDetrembleur <arthur.detrembleur@student.hepl.be>
2020-11-29 14:25:14 +0100

    Initial project ecommerce

---
+++ Ecommerce/src/main/java/com/bourgedetrembleur/hepl/HeplApplication.java
@@ -0,0 +1,15 @@
+package com.bourgedetrembleur.hepl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeplApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HeplApplication.class, args);
+    }
+
+}
---
+++ Ecommerce/src/test/java/com/bourgedetrembleur/hepl/HeplApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bourgedetrembleur.hepl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeplApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
